994293a92d222f330f6cf84315fddae031837ced
DEADushka108 <cloxx108@gmail.com>
2021-07-25 01:16:41 +0500

    'create'

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCJKazdnIiZP1kcRWeuI9jniZbJwsQgVuw',
+  fbDbUrl: 'https://angular-blog-96d9c-default-rtdb.europe-west1.firebasedatabase.app'
+};
